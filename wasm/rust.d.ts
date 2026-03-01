967ea2dd8f6f2e89de6f9a3414a19d35da3d23d5
kamike <minaken0584@gmail.com>
2026-03-01 13:50:17 +0900

    Updates

---
+++ wasm/rust.d.ts
@@ -0,0 +1,76 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} input
+* @param {string} output
+* @returns {number}
+*/
+export function get_max_turn(input: string, output: string): number;
+/**
+* @param {number} seed
+* @param {string} problem_id
+* @returns {string}
+*/
+export function gen(seed: number, problem_id: string): string;
+/**
+* @param {string} input
+* @param {string} output
+* @param {number} turn
+* @returns {Ret}
+*/
+export function vis(input: string, output: string, turn: number): Ret;
+/**
+*/
+export class Ret {
+  free(): void;
+/**
+*/
+  err: string;
+/**
+*/
+  score: bigint;
+/**
+*/
+  svg: string;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_get_ret_err: (a: number, b: number) => void;
+  readonly __wbg_get_ret_score: (a: number) => number;
+  readonly __wbg_get_ret_svg: (a: number, b: number) => void;
+  readonly __wbg_ret_free: (a: number) => void;
+  readonly __wbg_set_ret_err: (a: number, b: number, c: number) => void;
+  readonly __wbg_set_ret_score: (a: number, b: number) => void;
+  readonly __wbg_set_ret_svg: (a: number, b: number, c: number) => void;
+  readonly gen: (a: number, b: number, c: number, d: number) => void;
+  readonly get_max_turn: (a: number, b: number, c: number, d: number) => number;
+  readonly vis: (a: number, b: number, c: number, d: number, e: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
